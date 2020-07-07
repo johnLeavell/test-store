1c3f22b9a9c4b667add3408e51dae36d8913bcc9
John Leavell <johnleavell@gmail.com>
2020-07-07 15:25:35 -0500

    updated folder structure and added mock data and images

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Home from './pages/HomePage';
+import About from './pages/AboutPage';
+import Products from './pages/ProductsPage';
+import Contact from './pages/ContactPage';
+import SingleProduct from './pages/SingleProductPage';
+import Default from './pages/Default.js';
+
+
+
 class App extends Component {
   render() {
     return (
-      <h1>hello from test-store</h1>
+      <h1>
+        hello from test-store
+      </h1>
     )
   }
 }
+
 
 export default App;
