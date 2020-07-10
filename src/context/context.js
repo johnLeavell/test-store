47236f64e8164b63a7d01edec7ed2b1b108662dd
John Leavell <johnleavell@gmail.com>
2020-07-09 20:33:05 -0500

    experimenting with context

---
+++ src/context/context.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+const ProductContext = React.createContext();
+
+class ProductProvider extends Component {
+    state = {
+        sidebarOpen: false,
+        cartOpen: false,
+    }
+    //handle sidebar
+    handleSidebar = () => {
+        this.setState({
+            sidebarOpen: !this.state.sidebarOpen
+        })
+    }
+    // handle cart
+    handleCart = () => {
+        this.setState({
+            cartOpen: !this.state.sidebarOpen
+        })
+    }
+
+    //close cart
+    closeCart = () => {
+        this.setState({
+            cartOpen: false
+        })
+    }
+    //open
+    openCart = () => {
+        this.setState({
+            cartOpen: true
+        })
+    }
+    render() {
+        return(
+        <ProductContext.Provider value={{
+            ...this.state,
+            handleSidebar: this.handleSidebar,
+            handleCart: this.handleCart,
+            closeCart: this.closeCart,
+            openCart: this.openCart
+        }}>
+            {this.props.children}
+        </ProductContext.Provider>
+        )
+    }
+}
+
+const ProductConsumer = ProductContext.Consumer;
+
+export {ProductProvider, ProductConsumer}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { ProductConsumer } from '../context';
 
 export default function HomePage() {
     return (
         <>
-         <h1>Hello From HomePage</h1>   
+            <ProductConsumer>
+                {value => {
+                    console.log('====================================');
+                    console.log(value);
+                    console.log('====================================');
+                    return  <h1>Hello From HomePage</h1>
+                }}
+            </ProductConsumer>
         </>
     )
 }
