457bcd53c670eb0d668350313e5711bad0661115
John Leavell <johnleavell@gmail.com>
2020-07-11 12:35:13 -0500

    added logo image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { FaBars, FaCartPlus } from 'react-icons/fa';
+import styled from 'styled-components';
+import { ProductConsumer } from '../context';
+import logo from '../images/logo.svg';
+
 
 export default function Navbar() {
     return (
-        <>
-            Hello from Navbar   
-        </>
+            <ProductConsumer>
+                {value => {
+                    const {cartItems, handleSidebar, handleCart } = value
+                    return <nav>Hello navbar</nav>
+                }}
+            </ProductConsumer>
     )
 }
